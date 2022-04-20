d6aa8ff8862d286da4ed3a35c6e33a256a013616
Frederick-YTLPS <80813672+Frederick-YTLPS@users.noreply.github.com>
2022-04-20 15:44:20 +0800

    Update sast.cs

--- GoatlyCore/sast.cs
+++ GoatlyCore/sast.cs
@@ -1,3 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using vulnerable_asp_net_core.Models;
+using System.Data.SQLite;
+using System.IO;
+using System.Net;
+using System.Text.Encodings.Web;
+using System.Xml;
+using System.Xml.Serialization;
+
 XmlDocument parser = new XmlDocument();
 parser.XmlResolver = new XmlUrlResolver(); // Noncompliant: XmlDocument.XmlResolver configured with XmlUrlResolver that makes it unsafe
 parser.LoadXml("xxe.xml");
